926ebd6fe5fc2ecf26d538cf759ec3a2124fce75
mohit83k <mohit83k@gmail.com>
2020-06-01 20:11:14 +0530

    parsable blockchain

--- blockchain/block/block.go
+++ blockchain/block/block.go
@@ -1,10 +1,30 @@
 //block defines the block for the block chain and it's properties
 package block
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/rand"
+)
+
 type Block struct {
-	Index    string
-	Previous string
-	Hash     string
-	Data     string
-	Nounce   int
+	Index     int64
+	Previous  string
+	Hash      string
+	Data      string
+	Nounce    string
+	TimeStamp string
+}
+
+func (bl Block) GetHash() string {
+	sum := sha256.Sum256([]byte(bl.Previous + bl.Data + bl.TimeStamp + bl.Nounce))
+	return fmt.Sprint(sum)
+}
+
+func (bl *Block) Mine() {
+	//uses set of criteria to determine if it is a valid block
+	//For now : it just adds a random nounce. Extremely bad
+	bl.Nounce = string(rand.Int())
+	bl.Hash = bl.GetHash()
+
 }
--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,12 +2,55 @@ package blockchain
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/mohit83k/logiQ/blockchain/block"
+	block "github.com/mohit83k/logiQ/blockchain/block"
 )
 
-func say() {
-	b := new(block.Block)
-	b.nounce = 1
-	fmt.Println(block)
+var Blockchain []block.Block
+
+func init() {
+	//Add Genesis block
+	genesis := block.Block{}
+	genesis.Index = 0
+	genesis.Previous = "00000000000000000000000000000000"
+	genesis.Data = "Block chain for Logistics Author : Mohit Kumar" +
+		"ID : github.com/mohit83k"
+	genesis.Nounce = string(0)
+	genesis.TimeStamp = fmt.Sprint(time.Now())
+	genesis.Hash = genesis.GetHash()
+	appendToChain(genesis)
+}
+
+func AddData(data string) block.Block {
+	bl := getBlock(data)
+	bl.Mine()
+	appendToChain(bl)
+	return bl
+}
+
+func getBlock(data string) block.Block {
+	bl := block.Block{}
+	bl.Data = data
+	bl.TimeStamp = fmt.Sprint(time.Now())
+	bl.Previous = Blockchain[len(Blockchain)-1].Hash
+	bl.Index = Blockchain[len(Blockchain)-1].Index + 1
+	return bl
+}
+
+func appendToChain(bl block.Block) {
+	Blockchain = append(Blockchain, bl)
+}
+
+func AddBlock(bl block.Block) {
+	if bl.Previous == Blockchain[bl.Index-1].Hash {
+		appendToChain(bl)
+	}
+}
+
+func Exists(bl block.Block) bool {
+	if bl.Index < int64(len(Blockchain)) {
+		return true
+	}
+	return false
 }
